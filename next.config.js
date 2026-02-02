99da05ee0a259df676c6bf84fef9609cb90d8500
Jagatjyoti Tuladhar <jagatjyoti@fusemachines.com>
2026-02-02 16:06:42 +0545

    updated assetPrefix in next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   basePath: '/kasari-campaign',
-  assetPrefix: '/kasari-campaign/',
+  assetPrefix: '/kasari-campaign',
   trailingSlash: true,
 };
 
