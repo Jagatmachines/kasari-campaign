4029d422ea8085c083f52f7aa1b4ca0da7ea9563
Jagatjyoti Tuladhar <jagatjyoti@fusemachines.com>
2026-02-02 15:52:37 +0545

    updating for image not loading

--- next.config.js
+++ next.config.js
@@ -5,8 +5,9 @@ const nextConfig = {
     unoptimized: true,
   },
   reactStrictMode: true,
-  basePath: '/kasari-campaign', // Add your repository name here, including the leading slash
-  assetPrefix: '/kasari-campaign/', // Optional: can also be used for static assets
+  basePath: '/kasari-campaign',
+  assetPrefix: '/kasari-campaign/',
+  trailingSlash: true,
 };
 
 export default nextConfig;
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -53,9 +53,8 @@ export function Hero() {
           {/* Right Logo */}
           <div className="relative flex items-center justify-center">
             <div className="relative">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
               <Image
-                src="/images/kasari-logo.png"
+                src="images/kasari-logo.png"
                 alt="कसरी? From Promise to Policy"
                 className="w-full max-w-md h-auto"
               />
